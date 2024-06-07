cc50355838e2ba1f345a5956ef2018cdf5d8e2dc
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-06-06 21:17:33 -0400

    Streaming images 10 times

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,59 +1,57 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { ImageMessage } from './proto/image'
+import { ImageMessage } from "./proto/image";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [messages, setMessages] = useState<string[]>([]);
   const [imageSrc, setImageSrc] = useState<string | null>(null);
 
-  function appendMessage(message: string) {
-    setMessages((prevMessages) => {
-      var messages = [...prevMessages, message];
-      return messages;
-    });
-  }
-
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:12345?param1=THE_VALUE_OF_PARAM1');
+    const ws = new WebSocket("ws://localhost:12345?param1=THE_VALUE_OF_PARAM1");
     ws.onopen = () => {
-      console.log('Connected to server');
-      // appendMessage('Connected to server');
-      ws.send('Hello, server!');
+      console.log("Connected to server");
+      // Send a message to the server to trigger it
+      // to stream the image through websocket.
+      ws.send("Hello, server!");
     };
-    
+
     ws.onmessage = (event: any) => {
-      console.log(`Received message from server: ${event.data}`);
+      console.log("Received message from server");
       const reader = new FileReader();
       reader.readAsArrayBuffer(event.data);
       reader.onload = () => {
         const arrayBuffer = reader.result as ArrayBuffer;
         const byteArray = new Uint8Array(arrayBuffer);
 
-        // Deserialize the binary data into an Image message
+        // Deserialize the binary data into an ImageMessage message.
         const imageMsg = ImageMessage.decode(byteArray);
         console.log(`messageUuid: ${imageMsg.messageUuid}`);
         console.log(`description: ${imageMsg.description}`);
 
-        // Convert the binary content to a Blob and create a data URL
-        const blob = new Blob([imageMsg.binContent.data], { type: 'image/jpeg' });
+        // Convert the binary content to a Blob and create a data URL.
+        const blob = new Blob([imageMsg.binContent.data], {
+          type: "image/jpeg",
+        });
         const imageUrl = URL.createObjectURL(blob);
         setImageSrc(imageUrl);
       };
-      // appendMessage(`Received message from server: ${event.data}`);
     };
-    
+
     ws.onclose = () => {
-      console.log('Disconnected from server');
-      // appendMessage('Disconnected from server');
+      console.log("Disconnected from server");
     };
   }, []);
 
   return (
     <>
-      <div>{imageSrc ? <img src={imageSrc} alt="Received" /> : <p>No image received yet.</p>}</div>
-      <div><button onClick={() => {appendMessage("YAY!")}}>click</button></div>
+      <div>
+        {imageSrc ? (
+          <img src={imageSrc} alt="Received" />
+        ) : (
+          <p>No image received yet.</p>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,55 @@
-import * as WebSocket from 'ws';
-import * as fs from 'fs';
-import * as url from 'url'
-
-import { ImageMessage } from './proto/image';
+import * as WebSocket from "ws";
+import * as fs from "fs";
+import * as url from "url";
+import { ImageMessage } from "./proto/image";
 
 const wss = new WebSocket.Server({ port: 12345 });
-wss.on('connection', (ws: WebSocket, req) => {
-    const query = new URLSearchParams(url.parse(req.url).query)
-    console.log('New client connected:');
-    console.log(`   param1 presents : ${query.has("param1")}`)
-    console.log(`   param1 Value    : ${query.get("param1")}`)
+wss.on("connection", (ws: WebSocket, req) => {
+  const query = new URLSearchParams(url.parse(req.url).query);
+  console.log("New client connected:");
+  console.log(`   param1 presents : ${query.has("param1")}`);
+  console.log(`   param1 Value    : ${query.get("param1")}`);
 
-    ws.on('message', (message: string) => {
-        const fileData = fs.readFileSync("src/red_bunny.jpg");
-        const byteArray = new Uint8Array(fileData);
-        const imageMsg: ImageMessage = ImageMessage.create({
-            messageUuid: 123,
-            description: "dsafjpods",
-            binContent: {
-                data: byteArray
-            }
-        });
-        
-        // Serialize the protobuf message to binary format
-        const binaryData = ImageMessage.encode(imageMsg).finish();
-        ws.send(binaryData);
-    });
-    
-    ws.on('close', () => {
-            console.log('Client disconnected');
-    });
+  ws.on("message", (message: string) => {
+    const sendImage = (
+      filename: string,
+      messageUuid: number,
+      description: string
+    ) => {
+      const fileData = fs.readFileSync(filename);
+      const byteArray = new Uint8Array(fileData);
+      const imageMsg: ImageMessage = ImageMessage.create({
+        messageUuid: messageUuid,
+        description: description,
+        binContent: {
+          data: byteArray,
+        },
+      });
+
+      // Serialize the protobuf message and send it through websocket.
+      const binaryData = ImageMessage.encode(imageMsg).finish();
+      ws.send(binaryData);
+    };
+
+    var images = [
+      { filename: "src/red_bunny.jpg", id: 1, description: "Red Bunny" },
+      { filename: "src/puppy.jpg", id: 2, description: "Puppy" },
+      { filename: "src/bear_cub.jpg", id: 3, description: "Bear" },
+    ];
+    var theLoop: (i: number) => void = (i: number) => {
+      setTimeout(() => {
+        const index: number = i % images.length;
+        const { filename, id, description } = images[index];
+        sendImage(filename, id, description);
+        if (--i) {
+          theLoop(i);
+        }
+      }, 3000);
+    };
+    theLoop(10);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
 });
